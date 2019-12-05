fa2a4cd4cd23886fdf6886794de3a0fd340d1269
lukasjaronis <jaronisluke13@gmail.com>
2019-12-05 15:32:38 -0500

    display template complete

---
+++ baseball-stadium-personnel/src/components/Display.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Display = props => {
+    return(
+        <div>
+        <div>
+        <span>Strike: </span>
+        </div>
+        <span>Ball: </span>
+        </div>
+    )
+}
+
+export default Display;
