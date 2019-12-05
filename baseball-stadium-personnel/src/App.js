bb173da0682655fee6ef2ff380fa3d1fab69421c
lukasjaronis <jaronisluke13@gmail.com>
2019-12-05 15:36:00 -0500

    handleStrikes logic done

--- baseball-stadium-personnel/src/App.js
+++ baseball-stadium-personnel/src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Display from './components/Display';
+import ButtonPanel from './components/ButtonPanel';
+
+export const addStrike = currentStrike => {
+  return currentStrike + 1;
+}
+
 class App extends Component {
 
 state = {
 
-  balls: '',
-  strikes: '',
-  foul: ''
+  balls: 0,
+  strikes: 0
 }
 
+handleStrikes = event => {
+  event.preventDefault();
+
+  this.setState({
+    strikes: addStrike(this.setState.strikes)
+  });
+
+if (this.state.strike > 2) {
+  this.setState({
+    strikes: 0
+  });
+}
+
+}
 
 
 
