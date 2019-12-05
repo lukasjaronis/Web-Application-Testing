b1200341a161ed64c9d911b67d1495dde4a95c73
lukasjaronis <jaronisluke13@gmail.com>
2019-12-05 16:37:32 -0500

    installed react testing library

--- baseball-stadium-personnel/src/App.test.js
+++ baseball-stadium-personnel/src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App, {addStrike} from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('App renders without crashing', () => {
+const component = render(<App />);
+console.log(component);
+})
+
+test('Expect function to add + 1', () => {
+  let actual;
+  let expected;
+  expect(actual).toBe(expected);
+  expect(addStrike(0)).toBe(1)
+})
