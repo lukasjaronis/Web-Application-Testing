8f3ca0eb0a459c67ea5a0b7d06c691fd21d5f9eb
lukasjaronis <jaronisluke13@gmail.com>
2019-12-05 15:31:09 -0500

    button panel template complete

---
+++ baseball-stadium-personnel/src/components/ButtonPanel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const ButtonPanel = props => {
+    return (
+        <div>
+        <button>Strike</button>
+        <button>Ball</button>
+        <button>Hit</button>
+        <button>Foul</button>
+        </div>
+    )
+}
+
+export default ButtonPanel;
